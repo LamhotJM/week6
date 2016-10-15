0758825abfc83e9096945a497271391f02cc1500
LamhotJM <lamhotjms@gmail.com>
2016-10-15 23:27:26 +0700

    Create outofindex.java

---
+++ outofindex.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week6;
+
+/**
+ *
+ * @author Lamhot
+ */
+public class outofindex {
+      public static void main(String[] args) {
+      String[] jenisMakanan={"Padang","Chinese","Korean","Japan"};
+          int i;
+      
+      for (i=0;i<=5;i++)
+          System.out.println(jenisMakanan[i]);
+      }
+    
+    
+}
